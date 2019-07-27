64615fa400bbf8f50c391a1100c5d4fa5aec7795
Marit Zelinsky <maritzelinsky@me.com>
2019-07-27 13:12:52 -0500

    base mode done!

--- client.js
+++ client.js
@@ -2,34 +2,70 @@ console.log('js');
 
 $(document).ready(readyNow);
 
+// global variables
+let totalMonthly = 0;
+let totalAnnual = 0;
+
 function readyNow() {
     // event listener
     $('#submitButton').on('click', addEmployee);
+
     // delete listener
-    $('#newEmployeeInput').on('click', '.deleteButton', removeEmployee);
+    $('#tBody').on('click', '.deleteButton', removeEmployee);
+    
+    // append total monthly to DOM
+    $('#totalMonthly').append(totalMonthly);
 }
 
-function addEmployee() {
-    $('#newEmployeeInput').append(
-        `<tr><td>` +
-            $('#firstNameInput').val() + `</td><td>` +
-            $('#lastNameInput').val() + `</td><td>` +
-            $('#idInput').val() + `</td><td>` +
-            $('#titleInput').val() + `</td><td><span>$</span>` +
-            $('#annualSalaryInput').val() +`</td><td>` +
-            `<button class="deleteButton">Delete</button>` +
-        `</td></tr>`);
 
-        
+function addEmployee() {
+    // define inputs
+    let firstName = $('#firstNameInput').val();
+    let lastName = $('#lastNameInput').val();
+    let id = $('#idInput').val();
+    let title = $('#titleInput').val();
+    let annualSalary = $('#annualSalaryInput').val();
+    
+    // append inputs
+    $('#tBody').append(
+        `<tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${id}</td>
+            <td>${title}</td >
+            <td>${annualSalary}</td>
+            <td><button class="deleteButton">Delete</button></td>
+        <tr>`
+    );
+
+    // add input annualSalary to global totalAnnual
+    totalAnnual += parseFloat($('#annualSalaryInput').val());
+
+    // divide totalAnnual by 12
+    totalMonthly = totalAnnual / 12;
+
+    // append updated totalMonthly
+    $('#totalMonthly').text(totalMonthly);
+    console.log(totalMonthly);
+    console.log(totalAnnual); 
+
+    // clear inputs
     $('#firstNameInput').val('');
     $('#lastNameInput').val('');
     $('#idInput').val('');
     $('#titleInput').val('');
     $('#annualSalaryInput').val('')
+
+    // if totalMonthly > $20,000, add red background color make totalMonthly
+    if (totalMonthly > 20000) {
+        $('#totalMonthly').parent().addClass("warning");
+    }
+    
 }
 
 function removeEmployee() {
-    console.log('delete clicked!');
+    // remove table row from DOM
     $(this).parent().parent().remove();
 
+    // subtract deleted employess annualSalary from totalAnnual
 }
