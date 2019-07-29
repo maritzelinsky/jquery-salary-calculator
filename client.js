a984747ea4b2c6ff4bc72fbb4670e383d525ce95
Marit Zelinsky <maritzelinsky@me.com>
2019-07-28 20:20:22 -0500

    delete button now updates total montlhy, wooooooo!

--- client.js
+++ client.js
@@ -44,10 +44,10 @@ function addEmployee() {
     // divide totalAnnual by 12
     totalMonthly = totalAnnual / 12;
 
-    // append updated totalMonthly
+    // append updated totalMonthly to DOM
     $('#totalMonthly').text(totalMonthly);
-    console.log(totalMonthly);
-    console.log(totalAnnual); 
+    // console.log('total monthly: ', totalMonthly);
+    // console.log('add: ', totalAnnual); 
 
     // clear inputs
     $('#firstNameInput').val('');
@@ -59,13 +59,23 @@ function addEmployee() {
     // if totalMonthly > $20,000, add red background color make totalMonthly
     if (totalMonthly > 20000) {
         $('#totalMonthly').parent().addClass("warning");
-    }
-    
+    }  
 }
 
 function removeEmployee() {
+    // find annualSalary to be deleted 
+    let deleteAnnualSalary = $(this).parents('tr').find('td:nth-child(5)').text();
+
+    // subtract deleted employees annualSalary from totalAnnual
+    totalAnnual -= parseFloat(deleteAnnualSalary);
+    // console.log(deleteAnnualSalary);
+
+    // divide totalAnnual by 12
+    totalMonthly = totalAnnual / 12;
+
+    // append updated totalMonthly to DOM
+    $('#totalMonthly').text(totalMonthly);
+    
     // remove table row from DOM
     $(this).parent().parent().remove();
-
-    // subtract deleted employess annualSalary from totalAnnual
 }
